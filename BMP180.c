2d9239728e9133b07095cf9bba3da51ca4bddf2f
Akshat Jawne <69530774+AkshatJawne@users.noreply.github.com>
2022-11-06 15:22:49 -0500

    Update file

--- BMP180.c
+++ BMP180.c
@@ -1,18 +1,9 @@
 /**
   ******************************************************************************
-
-  BMP180 LIBRARY for STM32 using I2C
-  Author:   ControllersTech
-  Updated:  26/07/2020
-
-  ******************************************************************************
   Copyright (C) 2017 ControllersTech.com
 
   This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
   of the GNU General Public License version 3 as published by the Free Software Foundation.
-  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-  or indirectly by this software, read more about this on the GNU General Public License.
-
   ******************************************************************************
 */
 
